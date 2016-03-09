5d445a9b2716d408fdd3224a2526eba6665b6809
Al Brown <al@1for.one>
2016-03-08 17:35:40 -0800

    Added dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,5 @@
 
 set -e
 
-# Add logstash as command if needed
-#if [ "${1:0:1}" = '-' ]; then
-#	set -- logstash "$@"
-#fi
 
-# Run as user "logstash" if the command is "logstash"
-#if [ "$1" = 'logstash' ]; then
-#	set -- gosu logstash "$@"
-#fi
-
-exec "$@"
+logstash agent -f /etc/logstash/conf.d/logstash.conf
